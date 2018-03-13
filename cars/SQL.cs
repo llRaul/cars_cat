c8d60633649d1ac5bcd7c64a73af73b43217e893
llRaul <red4eagle4@gmail.com>
2018-03-13 14:32:40 +0300

    Importing proj

---
+++ cars/SQL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cars
+{
+    class SQL
+    {
+        public List<string> GetStingListByQuery(string query)
+        {
+            List<string> ret = new List<string>();
+            using (SqlConnection db = new SqlConnection(Properties.Settings.Default.SQLConnectionString))
+            {
+                SqlCommand SQLquery = new SqlCommand(query, db);
+                SqlDataReader Reader;
+                try
+                {
+                    db.Open();
+                    Reader = SQLquery.ExecuteReader();
+                    SqlDataAdapter adapter = new SqlDataAdapter(SQLquery);
+                    DataTable table = new DataTable(string.Empty);
+                    db.Close();
+                    adapter.Fill(table);
+                    foreach (DataRow row in table.Rows)
+                        ret.Add((string)row.ItemArray[0]);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Database error: {e}");
+                    return ret;
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ cars/ViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace cars
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private List<string> _manufacturers;
+        public List<string> Manufacturers
+        {
+            get { return _manufacturers; }
+            set
+            {
+                _manufacturers = value;
+                OnPropertyChanged("Manufacturers");
+            }
+        }
+        private List<string> _models;
+        public List<string> Models
+        {
+            get { return _models; }
+            set
+            {
+                _models = value;
+                OnPropertyChanged("Models");
+            }
+        }
+        private ImageSource _photo;
+        public ImageSource Photo
+        {
+            get { return _photo; }
+            set
+            {
+                _photo = value;
+                OnPropertyChanged("Photo");
+            }
+        }
+        private string _carInfo;
+        public string CarInfo
+        {
+            get { return _carInfo; }
+            set
+            {
+                _carInfo = value;
+                OnPropertyChanged("CarInfo");
+            }
+        }
+        private string _selectedManufacturer;
+        public string SelectedManufacturer
+        {
+            get { return _selectedManufacturer; }
+            set
+            {
+                _selectedManufacturer = value;
+                Models = new List<string>(SQLManager.GetStingListByQuery($"select model from Cars where manufacturer = '{value}'"));
+            }
+        }
+        private string _selectedModel;
+        public string SelectedModel
+        {
+            get { return _selectedModel; }
+            set
+            {
+                _selectedModel = value;
+                CarInfo = SQLManager.GetStingListByQuery($"select top 1 info from Cars where model = '{value}'")[0];
+                try
+                {
+                    Photo = null;
+                    BitmapImage img = new BitmapImage();
+                    img.BeginInit();
+                    img.CacheOption = BitmapCacheOption.OnLoad;
+                    img.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                    img.UriSource = new Uri($"Images/{value}.jpg", UriKind.Relative);
+                    img.EndInit();
+                    Photo = img;
+                }
+                catch
+                {
+                    MessageBox.Show("Изображения нет");
+                }
+            }
+        }
+        private SQL SQLManager = new SQL();
+
+        public ViewModel()
+        {
+            Manufacturers = new List<string>(SQLManager.GetStingListByQuery("select distinct manufacturer from Cars"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; 
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
